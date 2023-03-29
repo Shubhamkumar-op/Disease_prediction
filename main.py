e6fd32fe1c06c7601662c1510b6f63a60b3dc908
Shubham Kumar Singh <98223531+Shubhamkumar-op@users.noreply.github.com>
2023-03-29 23:17:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 
-diabetes_model = pickle.load(open("D:/work/machine _learning/Diabetes_model.sav",'rb'))
-Heart_dieases_model = pickle.load(open("D:/work/machine _learning/Heart_model.sav",'rb'))
+diabetes_model = pickle.load(open("Diabetes_model.sav",'rb'))
+Heart_dieases_model = pickle.load(open("Heart_model.sav",'rb'))
 
 
 with st.sidebar:
