cd5c0d81efb32afbc496a78c38db982847ae940a
Shubham Kumar Singh <98223531+Shubhamkumar-op@users.noreply.github.com>
2023-05-04 22:05:26 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 
-diabetes_model = pickle.load(open("Diabetes_model.sav",'rb'))
-Heart_dieases_model = pickle.load(open("Heart_model.sav",'rb'))
-parkinsons_model = pickle.load(open("parkinson_model.sav",'rb'))
+diabetes_model = pickle.load(open("D:/work/machine _learning/Diabetes_model.sav",'rb'))
+Heart_dieases_model = pickle.load(open("D:/work/machine _learning/Heart_model.sav",'rb'))
+parkinsons_model = pickle.load(open("D:/work/machine _learning/parkinson_model.sav",'rb'))
 
 
 
@@ -81,43 +81,43 @@ if (selected == 'Heart Disease prediction'):
     col1, col2, col3 = st.columns(3)
     
     with col1:
-        age = st.text_input('Age')
+        age = st.number_input('Age')
         
     with col2:
-        sex = st.text_input('Sex')
+        sex=st.number_input("(1=Male,0=Female)")
         
     with col3:
-        cp = st.text_input('Chest Pain types')
+        cp = st.number_input('Chest Pain types')
         
     with col1:
-        trestbps = st.text_input('Resting Blood Pressure')
+        trestbps = st.number_input('Resting Blood Pressure')
         
     with col2:
-        chol = st.text_input('Serum Cholestoral in mg/dl')
+        chol = st.number_input('Serum Cholestoral in mg/dl')
         
     with col3:
-        fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl')
+        fbs = st.number_input('Fasting Blood Sugar > 120 mg/dl')
         
     with col1:
-        restecg = st.text_input('Resting Electrocardiographic results')
+        restecg = st.number_input('Resting Electrocardiographic results')
         
     with col2:
-        thalach = st.text_input('Maximum Heart Rate achieved')
+        thalach = st.number_input('Maximum Heart Rate achieved')
         
     with col3:
-        exang = st.text_input('Exercise Induced Angina')
+        exang = st.number_input('Exercise Induced Angina')
         
     with col1:
-        oldpeak = st.text_input('ST depression induced by exercise')
+        oldpeak = st.number_input('ST depression induced by exercise')
         
     with col2:
-        slope = st.text_input('Slope of the peak exercise ST segment')
+        slope = st.number_input('Slope of the peak exercise ST segment')
         
     with col3:
-        ca = st.text_input('Major vessels colored by flourosopy')
+        ca = st.number_input('Major vessels colored by flourosopy')
         
     with col1:
-        thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
+        thal = st.number_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
         
         
     heart_diagnosis = ''
